0b9c1e00c76249daa7ba6e84d2994df27715f878
shenglong.qian <shenglong.qian@gotogameday.com>
2018-02-07 10:36:28 +0800

    add view middleware

--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,22 @@
 exports.keys = 'qsl'
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+// 添加 view 配置
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+// add middleware robot
+exports.middleware = [
+  'robot'
+];
+// robot's configurations
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ]
+};
---
+++ tsrc/view/news/list.tpl
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Hacker News</title>
+
+  </head>
+  <body>
+    <div class="news-view view">
+      {% for item in list %}
+        <div class="item">
+          <a href="{{ item.url }}">{{ item.title }}</a>
+        </div>
+      {% endfor %}
+    </div>
+  </body>
+</html>
